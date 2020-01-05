e61dc66f0e3a2b0e8ad4a3a4da3ba93f4f509df9
saboyutaka <yutaka.paradigm.shift@gmail.com>
2020-01-05 10:55:43 +0900

    :+1: graphql endpointを実装

--- graphql/index.js
+++ graphql/index.js
@@ -1,16 +1,19 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const { ApolloServer, gql } = require('apollo-server-azure-functions');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.graphqlHandler = server.createHandler();
